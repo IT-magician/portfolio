26170e63194e177e0277673d53b54c43470d94d3
hoan123432 <hoan123432@naver.com>
2022-12-04 03:06:10 +0900

    뼈대 올림..
    CSS 작업은 거의 끝난거같고.. 내용 채워야할것같다.

---
+++ working/asset/js/header.js
@@ -0,0 +1,52 @@
+// Show Menu
+function showMenu(toggleId,navId) {
+    const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId)
+
+    if (toggle && nav){
+        toggle.addEventListener('click', function(e){
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+
+// remove menu mobile
+// const navLink = document.querySelectorAll(".nav_link")
+
+// navLink.forEach(n => n.addEventListener('click',function(e){
+//     const navMenu = document.getElementById('nav-menu')
+//     navMenu.classList.remove('show-menu')
+// }))
+
+// scroll sections active link
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+console.log(sectionId)
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            // css 선택자 설명
+            // https://wallel.com/css-%EC%86%8D%EC%84%B1-%EC%84%A0%ED%83%9D%EC%9E%90-%EC%A0%95%EB%A6%AC-css-attribute-selector/
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+        else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// change background header
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if (this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
---
+++ working/asset/js/main.js
@@ -0,0 +1,69 @@
+const mixer = mixitup('.portfolio_container', {
+    selectors: {
+        target: '.portfolio_content'
+    },
+    animation: {
+        duration: 300
+    }
+})
+
+
+
+
+
+const swiper = new Swiper('.activities_container', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    spaceBetween: 16,
+    grabCursor: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints:{
+        640:{
+            slidesPerView:2,
+        },
+        
+        1024:{
+            slidesPerView:3,
+        }
+    }
+  });
+
+  gsap.from(".home_img",{
+    opacity: 0,
+    duration: 2,
+    delay: .5,
+    x: 60
+  })
+  gsap.from(".home_data",{
+    opacity: 0,
+    duration: 2,
+    delay: .8,
+    y: -60
+  })
+  gsap.from(".home_greeting, .home_name, .home_profession, .home_btn",{
+    opacity: 0,
+    duration: 2,
+    delay: 1,
+    x: 25,
+    ease: 'expo.out',
+    stagger: .2
+  })
+  gsap.from(".nav_item, .nav_logo",{
+    opacity: 0,
+    duration: 1,
+    delay: .8,
+    y: -60,
+    stagger: .2
+  })
